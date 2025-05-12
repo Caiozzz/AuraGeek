f39a3d5df7b7c4aea291ae09979bcad8fc2a067a
Caio <kaioshim2@gmail.com>
2025-05-12 19:46:38 -0300

    Correção do caminho para redirecionamento

--- js/login.js
+++ js/login.js
@@ -1,6 +1,6 @@
 const loginForm = document.getElementById('login');
 
 loginForm.onsubmit = (event) => {
-   window.location.href = '../novo-produto.html';
+   window.location.href = './novo-produto.html';
    event.preventDefault();
 }
